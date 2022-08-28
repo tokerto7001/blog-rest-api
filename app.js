0475872328938c99b00fd346f37c20a5cd7c70b7
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2022-08-28 16:32:48 +0300

    app.js

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 require('dotenv').config();
 
 require('./db');
 
 require('./db/models');
 
+app.use(cors());
+
+app.use(cookieParser());
+
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+
+
+// set up a 404 error handler
+app.all("*", (req, res, next) => {
+    return res.status(404).send('NOT FOUND!!!')
+});
 
 module.exports = app;
